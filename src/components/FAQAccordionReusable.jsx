ee64c4dbea58ef974f0278db9709826e2a3e0d36
Clifton T Canady <clifton@ctwebdesignshop.com>
2025-10-23 13:34:37 -0400

    more traing course changes

--- src/components/FAQAccordionReusable.jsx
+++ src/components/FAQAccordionReusable.jsx
@@ -5,19 +5,10 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
-interface FAQItem {
-  question: string;
-  answer: string;
-}
-
-interface FAQAccordionReusableProps {
-  faqs: FAQItem[];
-}
-
-export default function FAQAccordionReusable({ faqs }: FAQAccordionReusableProps) {
+export default function FAQAccordionReusable({ faqs }) {
   return (
     <Accordion type="single" collapsible className="w-full">
-      {faqs.map((faq, index) => (
+      {faqs && faqs.map((faq, index) => (
         <AccordionItem key={`item-${index + 1}`} value={`item-${index + 1}`}>
           <AccordionTrigger>{faq.question}</AccordionTrigger>
           <AccordionContent>{faq.answer}</AccordionContent>
